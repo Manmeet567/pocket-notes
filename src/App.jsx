e4363f772d8d87be21a02b397287677dae42a0d4
Manmeet567 <manmeet567singh@gmail.com>
2024-10-04 18:42:23 +0530

    add: added modal functionality, saving all the group data in local storage, style: added style to display groups in the sidebad

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
-import { useState,useEffect } from 'react'
-import Sidebar from './components/Sidebar/Sidebar'
-import StaticMainSection from './components/StaticMainSection/StaticMainSection'
-import Modal from './components/CreateGroup/Modal';
-import { v4 as uuidv4 } from 'uuid';
+import { useState, useEffect } from "react";
+import Sidebar from "./components/Sidebar/Sidebar";
+import StaticMainSection from "./components/StaticMainSection/StaticMainSection";
+import Modal from "./components/CreateGroup/Modal";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
-
   const [openModal, setOpenModal] = useState(false);
   const [groups, setGroups] = useState([]);
 
   useEffect(() => {
-    const storedGroups = JSON.parse(localStorage.getItem('groups'));
+    const storedGroups = JSON.parse(localStorage.getItem("groups"));
     if (storedGroups) {
       setGroups(storedGroups);
     }
@@ -25,18 +24,20 @@ function App() {
     const updatedGroups = [...groups, newGroup]; // Add new group to the groups array
 
     setGroups(updatedGroups);
-    localStorage.setItem('groups', JSON.stringify(updatedGroups)); // Save updated groups to local storage
+    localStorage.setItem("groups", JSON.stringify(updatedGroups)); // Save updated groups to local storage
     setOpenModal(false); // Close the modal after creation
   };
 
   return (
-    <div className='app'>
-        <Sidebar setOpenModal={setOpenModal} />
-        <StaticMainSection />
+    <div className="app">
+      <Sidebar setOpenModal={setOpenModal} groups={groups} />
+      <StaticMainSection />
 
-        {openModal && <Modal setOpenModal={setOpenModal} onCreateGroup={handleCreateGroup} />}
+      {openModal && (
+        <Modal setOpenModal={setOpenModal} onCreateGroup={handleCreateGroup} />
+      )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,6 +1,14 @@
 import "./Sidebar.css";
 
-function Sidebar({setOpenModal}) {
+function Sidebar({setOpenModal, groups}) {
+
+  const getGroupIcon = (name) => {
+    const words = name.split(" ");
+    const firstLetter = words[0][0].toUpperCase();
+    const lastLetter = words[words.length - 1][0].toUpperCase();
+    return firstLetter + lastLetter;
+  };
+
   return (
     <div className="sidebar">
       <div className="title">
@@ -8,7 +16,14 @@ function Sidebar({setOpenModal}) {
       </div>
 
       <div className="notes-group-list">
-        <div className="group-list"></div>
+        <div className="group-list">
+          { groups.map((group) => (
+            <div className="group" key={group.id}>
+              <div className="group-icon" style={{backgroundColor:`${group.color}`}}>{getGroupIcon(group.name)}</div>
+              <div className="group-title">{group?.name}</div>
+            </div>
+          ))}
+        </div>
       </div>
 
       <div onClick={() => setOpenModal(true)} className="add-icon">+</div>
